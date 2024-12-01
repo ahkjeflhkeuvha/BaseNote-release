64ee50adf5c401e8c31e4f0319ca9652cc0e9a15
오지은 <w2317@e-mirim.hs.kr>
2024-12-01 18:59:33 +0900

    fix jsonPosts

--- js/community.js
+++ js/community.js
@@ -5,9 +5,7 @@ function loadPosts() {
     // localStorage에서 데이터 가져오기
     let localPosts = JSON.parse(localStorage.getItem('posts')) || [];
     
-    const allPosts = [...localPosts, ...jsonPosts];
 
-    console.log(allPosts)
 
     // post.json 파일에서 데이터 가져오기
     fetch('post.json')
@@ -20,7 +18,9 @@ function loadPosts() {
         .then(jsonPosts => {
             // JSON 게시물과 localStorage 게시물을 합칩니다.
 
+            const allPosts = [...localPosts, ...jsonPosts];
 
+            console.log(allPosts)
             // 모든 게시물을 리스트에 추가
             allPosts.forEach(post => {
                 const postItem = document.createElement('div');
